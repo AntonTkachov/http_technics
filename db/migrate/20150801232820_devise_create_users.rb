8967eaa37c0b901c6cfad330ec9e72a14eacce95
Anton Tkachov <lookformeon@gmail.com>
2015-08-02 02:41:38 +0300

    Initial

---
+++ db/migrate/20150801232820_devise_create_users.rb
@@ -0,0 +1,11 @@
+class DeviseCreateUsers < ActiveRecord::Migration
+  def change
+    create_table(:users) do |t|
+      ## Database authenticatable
+      t.string :login,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      t.timestamps null: false
+    end
+  end
+end
