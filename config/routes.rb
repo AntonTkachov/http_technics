7fba8ce0aa648123060552daedf8fcfe0b1079dc
Anton Tkachov <lookformeon@gmail.com>
2015-08-03 13:33:17 +0300

    Customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'devise/sessions#new'
+  scope :users do
+    get    'sign_up', controller: 'users/registrations', action: 'new', as: 'new_user_registration'
+    post   '/', controller: 'users/registrations', action: 'create', as: 'user_registration'
+    get    'sign_in', controller: 'users/sessions', action: 'new', as: 'new_user_session'
+    post   'sign_in', controller: 'users/sessions', action: 'create', as: 'user_session'
+    delete 'sign_out', controller: 'users/sessions', action: 'destroy', as: 'destroy_user_session'
+  end
+  root to: 'home#index'
 end
