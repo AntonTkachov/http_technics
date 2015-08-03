3a93117470f801e934555af44718e4c18a2e4762
Anton Tkachov <lookformeon@gmail.com>
2015-08-03 14:21:27 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  scope :users do
-    get    'sign_up', controller: 'users/registrations', action: 'new', as: 'new_user_registration'
-    post   '/', controller: 'users/registrations', action: 'create', as: 'user_registration'
-    get    'sign_in', controller: 'users/sessions', action: 'new', as: 'new_user_session'
-    post   'sign_in', controller: 'users/sessions', action: 'create', as: 'user_session'
-    delete 'sign_out', controller: 'users/sessions', action: 'destroy', as: 'destroy_user_session'
+  devise_scope :user do
+    scope :users do
+      get 'sign_in', controller: 'users/sessions', action: 'new', as: 'new_user_session'
+      post '/', controller: 'users/registrations', action: 'create', as: 'user_registration'
+      post 'sign_in', controller: 'users/sessions', action: 'create', as: 'user_session'
+      delete 'sign_out', controller: 'users/sessions', action: 'destroy', as: 'destroy_user_session'
+    end
+    root to: 'users/sessions#new'
   end
-  root to: 'home#index'
 end
